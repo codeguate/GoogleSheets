d357a69869f90aa03f1a2ff4769c7d5905dce69f
Daniel Rodriguez <dannyjose1112@hotmail.com>
2019-08-12 20:49:40 -0600

    [UPDATED] added

--- setDataSheet.php
+++ setDataSheet.php
@@ -8,6 +8,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 define('APPLICATION_NAME', 'Nissan');
 define('CLIENT_SECRET_PATH', __DIR__ . '/key.json');
 define('CREDENTIALS_PATH',  __DIR__ .'/nissanform-ed5a14e568ed.json');
+
+
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-php-quickstart.json
     $client = new \Google_Client();
@@ -22,10 +24,45 @@ define('CREDENTIALS_PATH',  __DIR__ .'/nissanform-ed5a14e568ed.json');
     $nombre = $_GET['nombre'];
     $apellido = $_GET['apellido'];
     $agencia = $_GET['agencia'];
+    $agenciaN = "";
     $email = $_GET['email'];
     $telefono = $_GET['telefono'];
+    switch($agencia){
+        case "Liberación":{
+            $agenciaN = "14";
+            break;
+        }
+        case "Roosevelt":{
+            $agenciaN = "03";
+            break;
+        }
+        case "Condado Concepción":{
+            $agenciaN = "16";
+            break;
+        }
+        case "Quetzaltenango":{
+            $agenciaN = "17";
+            break;
+        }
+        case "Agencia Peten":{
+            $agenciaN = "19";
+            break;
+        }
+        case "Teculutan":{
+            $agenciaN = "20";
+            break;
+        }
+    }
+    // $agenciasH = (object)array(
+    //     "Liberación"=>"14",
+    //     "Roosevelt"=>"03",
+    //     "Condado Concepción"=>"16",
+    //     "Quetzaltenango"=>"17",
+    //     "Agencia Peten"=>"19",
+    //     "Teculutan"=>"20"
+    // );
     $values = [
-        [$nombre,$apellido,$email,$telefono,$agencia]
+        [$nombre,$apellido,$email,$telefono,$agenciaN]
     ];
     $body = new Google_Service_Sheets_ValueRange([
         'values' => $values
